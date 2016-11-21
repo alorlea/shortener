27d7b148bf4999161886345dc603c203638d5cab
Alberto Lorente <alberto@izettle.com>
2016-11-21 13:06:22 +0100

    adding test for shortener engine

---
+++ shortener-service/src/test/java/engine/ShortenerEngineTest.java
@@ -0,0 +1,33 @@
+package engine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by alberto on 2016-11-21.
+ */
+public class ShortenerEngineTest {
+
+    private ShortenerEngine shortenerEngine;
+
+    @Before
+    public void setup(){
+        this.shortenerEngine = new ShortenerEngine("http://localhost", new Base62(), 8);
+    }
+
+    @Test
+    public void encodingURLGivesShortURL(){
+        String shortUrl = shortenerEngine.encodeURL("http://dice.se");
+        assertEquals("alilnDyEW", shortUrl);
+    }
+
+    @Test
+    public void differentURLsGiveDifferentResults(){
+        String shortUrl = shortenerEngine.encodeURL("http://dice.se");
+        String other = shortenerEngine.encodeURL("http://www.google.com");
+        assertNotEquals(shortUrl, other);
+    }
+
+}
